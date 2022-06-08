d5f90b96561b2139250420818e11758d66fed64d
Kevin <kevin.rodriguez678@gmail.com>
2022-06-08 16:27:35 -0400

    fix text behind image

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-let hoverOver =document.getElementById('secret')
+const hoverOver =document.querySelector('.secret')
+const desc = document.querySelector('.imgDesc')
+
 
 hoverOver.addEventListener('mouseover', function(evt){
   evt.target.style.opacity = ".1"
+  // desc.style.display = "I like trtles!!"
 })
 
 hoverOver.addEventListener('mouseout', function(evt){
   evt.target.style.opacity = ""
-})
+  // desc.style.display = 'none'
+  })
