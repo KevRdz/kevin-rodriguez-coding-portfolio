9ae6d87c73a35c59bd6242f8647f0bee8b352fc2
Kevin <kevin.rodriguez678@gmail.com>
2022-06-08 14:58:23 -0400

    add mouseover and mouseout

--- js/app.js
+++ js/app.js
@@ -4,3 +4,6 @@ hoverOver.addEventListener('mouseover', function(evt){
   evt.target.style.opacity = ".1"
 })
 
+hoverOver.addEventListener('mouseout', function(evt){
+  evt.target.style.opacity = ""
+})
