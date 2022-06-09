004fdc88307b185e008772b7cec0697cd8ee2760
Kevin <kevin.rodriguez678@gmail.com>
2022-06-08 23:29:17 -0400

    add card to page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 const hoverOver =document.querySelector('.secret')
-const desc = document.querySelector('.imgDesc')
+const darkBtn = document.querySelector('.dark-mode')
 
 
 hoverOver.addEventListener('mouseover', function(evt){
   evt.target.style.opacity = ".1"
-  // desc.style.display = "I like trtles!!"
 })
 
 hoverOver.addEventListener('mouseout', function(evt){
   evt.target.style.opacity = ""
-  // desc.style.display = 'none'
+  })
+
+  darkBtn.addEventListener('click', function(evt){
+    
   })
