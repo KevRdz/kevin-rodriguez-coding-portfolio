dc62978893747914d4fa1f8b973c7fb7878078db
Kevin <kevin.rodriguez678@gmail.com>
2022-06-30 22:00:23 -0400

    add my second project

--- js/project-data.js
+++ js/project-data.js
@@ -5,6 +5,13 @@ let projectData = [
     image: '/assets/images/ttt.png',
     github: 'https://github.com/UncleHagi/ttt-weekend',
     description: 'Fun game to play with friends and family'
+  },
+  {
+    title: 'Fish Tracker',
+    deployment: "https://fish-tracker.herokuapp.com/",
+    image: '/assets/images/Fish-tracker.png',
+    github: 'https://github.com/UncleHagi/fish-tracker',
+    description: "A site created to help fishermen track fishes they've caught"
   }
 ]
 
