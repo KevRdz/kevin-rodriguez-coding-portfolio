1243955195946af6b5d40f33d364389b365facef
Kevin <kevin.rodriguez678@gmail.com>
2022-08-11 23:49:10 -0400

    add projects to my projects page

--- js/project-data.js
+++ js/project-data.js
@@ -12,7 +12,21 @@ let projectData = [
     image: '/assets/images/Fish-tracker.png',
     github: 'https://github.com/UncleHagi/fish-tracker',
     description: "A site created to help users track fishes."
-  }
+  },
+  {
+    title: 'Newt',
+    deployment: "https://newt-activity.netlify.app/",
+    image: '/assets/images/newt.png',
+    github: 'https://github.com/UncleHagi/newt-front-end.git',
+    description: "A fun webapp designed to give activity ideas to users."
+  },
+  {
+    title: 'Hoarder of Cards',
+    deployment: "https://hoarder-of-cards.herokuapp.com/",
+    image: '/assets/images/hoarder-of-cards.png',
+    github: 'https://github.com/UncleHagi/hoarder-of-cards.git',
+    description: "Useful webapp to log and keep track of card collections."
+  },
 ]
 
 export{
